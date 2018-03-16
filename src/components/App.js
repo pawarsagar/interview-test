1a8ec481ed316d1bfdcbf4571ecf43cebc2d01f2
pawarsagar <pawar.sagar875.sp@gmail.com>
2018-03-16 12:40:51 +0530

    changed the text of button

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const App = () => {
-  return <button className="btn btn-primary">Hello World!</button>
+  return <button className="btn btn-primary">Hello !</button>
 }
 
 export default App
